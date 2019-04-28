86f8f80d85a16870e5af5d780a3412d73267f68f
Matthew Hough <matthough1@icloud.com>
2019-04-28 15:22:53 -0400

    break up repository & add documentation

--- src/Message/MessageRepository.cs
+++ src/Message/MessageRepository.cs
@@ -18,30 +18,9 @@ namespace brassy_api.src.Message {
         public async Task<IEnumerable<MessageModel>> Get () {
             _logger.LogInformation ("Getting all messages...");
             var messages = await _db.Messages.ToListAsync ();
-            var modifiedMessages = messages.Select (msg => {
-                if (msg.Mood == MoodModel.ANGRY) {
-                    msg.Content = msg.Content.ToUpper ();
-                } else if (msg.Mood == MoodModel.NEUTRAL) {
-                    msg.Content = msg.Content.ToLower ();
-                } else if (msg.Mood == MoodModel.BORED) {
-                    string newString = "";
-                    for (var i = 0; i < msg.Content.Length; i++) {
-                        var random = new Random ();
-                        newString += random.Next (100) < 40 ?
-                            msg.Content[i]
-                            .ToString ()
-                            .ToLower () :
-                            msg.Content[i]
-                            .ToString ()
-                            .ToUpper ();
-                    }
-                    msg.Content = newString;
-                }
+            var formattedMessages = MessageRepository.FormatMessages (messages);
 
-                return msg;
-            });
-
-            return modifiedMessages;
+            return formattedMessages;
         }
 
         public async Task<MessageModel> AddMessage (MessageModel message) {
@@ -49,6 +28,46 @@ namespace brassy_api.src.Message {
             var addedMessage = await _db.Messages.AddAsync (message);
             await _db.SaveChangesAsync ();
             return addedMessage.Entity;
+        }
+
+        /// <summary>
+        /// Takes IEnumerable of MessageModels, and formats content of messages to MoodModel specified format.
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public static IEnumerable<MessageModel> FormatMessages (IEnumerable<MessageModel> messages) {
+            return messages.Select (msg => {
+                if (msg.Mood == MoodModel.ANGRY) {
+                    msg.Content = msg.Content.ToUpper ();
+                } else if (msg.Mood == MoodModel.NEUTRAL) {
+                    msg.Content = msg.Content.ToLower ();
+                } else if (msg.Mood == MoodModel.BORED) {
+                    msg.Content = MessageRepository.RandomlyCapitalize (msg.Content);
+                }
+
+                return msg;
+            });
+        }
+
+        /// <summary>
+        /// Takes a string and randomly capitalizes its letters.
+        /// </summary>
+        /// <param name="msg">string</param>
+        /// <returns>string with random letters capitalized.</returns>
+        public static string RandomlyCapitalize (string msg) {
+            string newString = "";
+            for (var i = 0; i < msg.Length; i++) {
+                var random = new Random ();
+                newString += random.Next (100) < 40 ?
+                    msg[i]
+                    .ToString ()
+                    .ToLower () :
+                    msg[i]
+                    .ToString ()
+                    .ToUpper ();
+            }
+
+            return newString;
         }
     }
 }
