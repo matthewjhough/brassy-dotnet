4f02f1fea202268c43c406abee4e9df91fd557a1
Matthew Hough <matthough1@icloud.com>
2019-04-28 14:32:36 -0400

    update message type

--- src/Message/MessageType.cs
+++ src/Message/MessageType.cs
@@ -5,6 +5,7 @@ namespace brassy_api.src.Message {
         public MessageType () {
             Field (x => x.Id).Description ("The Id of the Message");
             Field (x => x.Content, nullable : false).Description ("The user input Content of the Message.");
+            Field (x => x.Mood, nullable : false).Description ("the current state of mind or feeling that will alter the appearance of content.");
         }
     }
 }
